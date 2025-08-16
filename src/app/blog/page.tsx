db992ea25147b708a6fa50a5f04838deab7fe359
Abhijeet <abhijeetsachan.088@gmail.com>
2025-08-16 13:23:10 +0530

    add blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <section className="p-10">
+      <h1 className="text-4xl font-bold">My Blog</h1>
+      <p className="mt-4 text-lg">Welcome to my blog! Here you will find articles on various topics...</p>
+    </section>
+  );
+}

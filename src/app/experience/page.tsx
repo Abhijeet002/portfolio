7979ae5553d732fcd05742ba74b52574a529b5a6
Abhijeet <abhijeetsachan.088@gmail.com>
2025-08-16 13:29:53 +0530

    add experience page

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,8 @@
+export default function Experience() {
+  return (
+    <section className="p-10">
+      <h1 className="text-4xl font-bold">My Experience</h1>
+      <p className="mt-4 text-lg">I have worked on various projects, including:</p>
+    </section>
+  );
+}

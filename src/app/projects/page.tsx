6ed34abd79c7dc47a64c8f8b026ed6e7c132ab5a
Abhijeet <abhijeetsachan.088@gmail.com>
2025-08-16 13:30:09 +0530

    add projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <section className="p-10">
+      <h1 className="text-4xl font-bold">My Projects</h1>
+      <p className="mt-4 text-lg">Here are some of the projects I&apos;ve worked on:</p>
+    </section>
+  );
+}

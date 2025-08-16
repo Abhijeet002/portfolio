10e7aa912790556908d807690c28b3c7ccaa9408
Abhijeet <abhijeetsachan.088@gmail.com>
2025-08-16 13:22:54 +0530

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section className="p-10">
+      <h1 className="text-4xl font-bold">About Me</h1>
+      <p className="mt-4 text-lg">I am Abhijeet, a Full Stack Developer...</p>
+    </section>
+  );
+}
